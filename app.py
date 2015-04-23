aa6eb2629b0a281eb361fc3ed1587cf0dddc3a99
Jon Rovira <jonathanrovira2016@u.northwestern.edu>
2015-04-23 17:39:24 -0500

    set up database with postgresql with migrations set up

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 import os
 
 
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
+db = SQLAlchemy(app)
+
+from models import Result
 
 
 
